188226e38fccb4083346858588052a21baa31d47
KopiRoasters <seantan17101997@gmail.com>
2020-05-16 01:07:07 +0800

    Create yfinance_scrape.py
    Storing stock details in DB

---
+++ yfinance_scrape.py
@@ -0,0 +1,55 @@
+import yfinance as yf
+import datetime
+import pymysql.cursors
+import json
+
+def DBconnection(sql_statement, input_data):
+    conn = pymysql.connect(
+        'database-1.cqifbqu4xgne.ap-southeast-1.rds.amazonaws.com', 'admin', 'password', 'XTASFinanceBot')
+
+    with conn:
+        cur = conn.cursor()
+        cur.execute(sql_statement, input_data)
+        rows = cur.fetchall()
+        return rows
+
+def get_stock_info(list_of_stocks):
+    main_dict = {}
+    for stocks in list_of_stocks:
+        try:
+            stock = yf.Ticker(stocks)
+        except:
+            return "error"
+        
+        L2_dict = {'52WeekChange': '', 'dividendYield': '', 'marketCap': '', 'currency': '', 'previousClose': '',
+                   'regularMarketOpen': '', 'regularMarketPrice': '', 'updated_as_of': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
+
+        for keystock, valuestock in stock.info.items():
+            for key in L2_dict.keys():
+                if keystock == key:
+                    L2_dict[key] = valuestock
+        main_dict[stocks] = L2_dict
+        if check_for_existing_stock(stocks):
+            sql_statement = """UPDATE stock_data SET data_stored = %s, last_updated = %s where stock_id = %s"""
+            sql_run = (json.dumps(L2_dict),datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), stocks)
+        else:
+            sql_statement = """INSERT INTO stock_data (stock_id, data_stored, last_updated) VALUES(%s,%s,%s)"""
+            sql_run = (stocks, json.dumps(L2_dict), datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        DBconnection(sql_statement, sql_run)
+
+        L2_dict = {'52WeekChange': '', 'dividendYield': '', 'marketCap': '', 'currency': '', 'previousClose': '',
+                   'regularMarketOpen': '', 'regularMarketPrice': '', 'updated_as_of': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
+
+    return main_dict
+
+
+def check_for_existing_stock(stock_id):
+    exist = True
+    sql_statement = """SELECT stock_id FROM stock_data where stock_id in (%s)"""
+    sql_run = (stock_id)
+    output = DBconnection(sql_statement, sql_run)
+    if output == ():
+        exist = False
+    return exist
+
+get_stock_info(['MSFT', 'GOOG', 'FB'])
